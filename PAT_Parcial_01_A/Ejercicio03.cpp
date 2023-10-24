dfbf0ae90fd8ac60652b43c223b100138cf6a0ff
dummy2004 <p.klee.4bdb@gmail.com>
2023-10-24 08:27:44 -0600

    este si primerodios

--- PAT_Parcial_01_A/Ejercicio03.cpp
+++ PAT_Parcial_01_A/Ejercicio03.cpp
@@ -1,18 +1,13 @@
-#include "Ejercicio03.h"
 #include <iostream>
 #include <climits>
 
+#include "Ejercicio03.h"
+
 MinStack::Node::Node(int val, int min_val, Node* next_node) : value(val), min_value(min_val), next(next_node) {}
 
-MinStack::MinStack() : top_node(nullptr) {}
-
-MinStack::MinStack() {
-    while (top_node != nullptr) {
-        Node* temp = top_node;
-        top_node = top_node->next;
-        delete temp;
-    }
+MinStack::MinStack() : top_node(nullptr) {
 }
+
 
 void MinStack::push(int value) {
     int min_val = (top_node == nullptr) ? value : std::min(value, top_node->min_value);
@@ -49,9 +44,10 @@ int main() {
     std::cout << "Min: " << minStack->getMin() << std::endl;
     minStack->pop();
     std::cout << "Top: " << minStack->top() << std::endl;
-    std::cout << "Min: " << minStack->getMin() << std::endl; 
+    std::cout << "Min: " << minStack->getMin() << std::endl;
 
     delete minStack;
 
     return 0;
 }
+
